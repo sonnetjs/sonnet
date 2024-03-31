630270c3e00d3d93c399595eb73f9d00081c98ba
Talha Mujahid <htmujahid@gmail.com>
2024-03-31 09:59:42 +0500

    feat: tailwindcss support

---
+++ packages/config-tailwind/tailwind.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "tailwindcss";
+
+// We want each package to be responsible for its own content.
+const config: Omit<Config, "content"> = {
+  theme: {},
+  plugins: [],
+};
+export default config;
