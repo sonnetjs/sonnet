2daf104268c0e5b3c364b978b9fd8cefecd88aec
htmujahid <htmujahid@gmail.com>
2024-04-03 15:36:47 +0500

    sonnet-core sourcemap disabled

--- packages/sonnet-core/tsup.config.ts
+++ packages/sonnet-core/tsup.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig(() => ({
-  entryPoints: ["src/index.ts"],
-  format: ["esm"],
+  entryPoints: ['src/index.ts'],
+  format: ['esm'],
   dts: true,
-  sourcemap: true,
+  sourcemap: false,
   minify: true,
 }));
