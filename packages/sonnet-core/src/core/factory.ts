968ac7f3ede3ce2fd16accf4dd1b3edfed6b6dfa
Talha Mujahid <htmujahid@gmail.com>
2024-05-13 08:05:55 +0500

    fix: remove functional component support

--- packages/sonnet-core/src/core/factory.ts
+++ packages/sonnet-core/src/core/factory.ts
@@ -1,52 +1,23 @@
-import { SonnetGet } from '@sonnetjs/shared';
-import SonnetComponent from '../abstract/SonnetComponent';
 import { EventEmitter } from './Event';
+import SonnetComponent from '../abstract/SonnetComponent';
 
 const event = EventEmitter.getInstance();
 
-interface Component {
+interface Component<T> {
+  new (args?: T): SonnetComponent;
   script?: () => void;
   staticScript?: () => void;
 }
 
-interface ComponentFunction<T> extends Component {
-  (args?: T): SonnetGet;
-}
-
-interface ComponentClass<T> extends Component {
-  new (args?: T): SonnetComponent;
-}
-
-export function $component<T>(
-  component: ComponentFunction<T> | ComponentClass<T>,
-) {
+export function $component<T>(component: Component<T>) {
   return (args?: T) => {
-    if (component.prototype instanceof SonnetComponent) {
-      // @ts-expect-error - this is a class
-      const instance = new component(args);
-      if (component.script && component.script.toString() !== 'script(){}') {
-        event.once('script', component.script);
-      }
-      if (instance.script) {
-        event.on('script', instance.script.bind(instance));
-      }
-      return instance as SonnetComponent;
+    const instance = new component(args);
+    if (component.script && component.script.toString() !== 'script(){}') {
+      event.once('script', component.script);
     }
-    if (component.staticScript) {
-      event.once('script', component.staticScript);
+    if (instance.script) {
+      event.on('script', instance.script.bind(instance));
     }
-    if (component.script) {
-      event.on('script', component.script);
-    }
-    const result = {
-      // @ts-expect-error - this is a function
-      get: () => component(args),
-      callback: (cb: (component: SonnetComponent) => void) => {
-        cb(result);
-        return result;
-      },
-    } as SonnetComponent;
-
-    return result;
+    return instance as SonnetComponent;
   };
 }
--- playgrounds/sonnet-function/src/main.ts
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import { createApp } from '@sonnetjs/core';
-import App from './App';
-
-// benchmark
-const app = createApp();
-app.root(App);
-app.mount('#app');
