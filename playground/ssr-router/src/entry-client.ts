4e4573a11ed8a4f4ad629ed344bc8e7ba3bd4137
htmujahid <htmujahid@gmail.com>
2024-10-02 22:25:12 +0500

    fix: ssr playground

--- playground/ssr-router/src/entry-client.ts
+++ playground/ssr-router/src/entry-client.ts
@@ -1,13 +1,13 @@
-// import './style.css';
-// import { createApp } from '@sonnetjs/core';
+import './style.css';
+import { createApp } from '@sonnetjs/core';
 
-// import App from './App';
-// import { routers } from './routes';
+import App from './App';
+import { routers } from './routes';
 
-// const app = createApp();
-// app.root(App);
-// app.use(routers);
-// app.lazy();
-// app.ssr();
-// app.mount('#app');
-// app.initialized();
+const app = createApp();
+app.root(App);
+app.use(routers);
+app.lazy();
+app.ssr();
+app.mount('#app');
+app.initialized();
